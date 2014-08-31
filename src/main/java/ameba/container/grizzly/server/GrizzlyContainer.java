a6817b5aef88527e83fea5a51d9cbe6c3fee0603
icode <intelligentcodemail@gmail.com>
2014-08-31 20:41:32 +0800

    增加多端口监听配置

--- src/main/java/ameba/container/grizzly/server/GrizzlyContainer.java
+++ src/main/java/ameba/container/grizzly/server/GrizzlyContainer.java
@@ -5,17 +5,12 @@ import ameba.container.Container;
 import ameba.exceptions.AmebaException;
 import ameba.mvc.assets.AssetsFeature;
 import org.apache.commons.lang3.StringUtils;
-import org.glassfish.grizzly.http.CompressionConfig;
 import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.ServerConfiguration;
-import org.glassfish.grizzly.ssl.SSLContextConfigurator;
-import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer;
 import org.glassfish.jersey.server.ContainerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -27,7 +22,6 @@ import java.util.concurrent.ExecutionException;
  * @author icode
  */
 public class GrizzlyContainer extends Container {
-    public static final Logger logger = LoggerFactory.getLogger(GrizzlyContainer.class);
 
     private HttpServer httpServer;
 
@@ -39,14 +33,10 @@ public class GrizzlyContainer extends Container {
         containerProvider = ContainerFactory.createContainer(GrizzlyHttpContainer.class, app);
 
         httpServer = GrizzlyServerFactory.createHttpServer(
-                app.getHttpServerBaseUri(),
                 containerProvider,
                 app.getProperties(),
-                getCompressionConfig(app),
-                app.isSecureEnabled(),
-                app.isAjpEnabled(),
+                app.getConnectors(),
                 app.isJmxEnabled(),
-                getSslEngineConfigurator(app),
                 false);
 
         ServerConfiguration serverConfiguration = httpServer.getServerConfiguration();
@@ -69,56 +59,6 @@ public class GrizzlyContainer extends Container {
         }
 
         httpServer.getServerConfiguration().setDefaultQueryEncoding(Charset.forName(charset));
-    }
-
-    private static CompressionConfig getCompressionConfig(ameba.Application app) {
-        CompressionConfig compressionConfig = new CompressionConfig();
-        String modeStr = (String) app.getProperty("http.compression.mode");
-        if (StringUtils.isNotBlank(modeStr) && ((modeStr = modeStr.toUpperCase()).equals("ON") || modeStr.equals("FORCE"))) {
-
-            String minSizeStr = (String) app.getProperty("http.compression.minSize");
-            String mimeTypesStr = (String) app.getProperty("http.compression.mimeTypes");
-            String userAgentsStr = (String) app.getProperty("http.compression.ignore.userAgents");
-
-            compressionConfig.setCompressionMode(CompressionConfig.CompressionMode.fromString(modeStr)); // the mode
-            if (StringUtils.isNotBlank(minSizeStr))
-                try {
-                    compressionConfig.setCompressionMinSize(Integer.parseInt(minSizeStr)); // the min amount of bytes to compress
-                } catch (Exception e) {
-                    logger.error("parse http.compression.minSize error", e);
-                }
-            if (StringUtils.isNotBlank(mimeTypesStr))
-                compressionConfig.setCompressableMimeTypes(mimeTypesStr.split(",")); // the mime types to compress
-            if (StringUtils.isNotBlank(userAgentsStr))
-                compressionConfig.setNoCompressionUserAgents(userAgentsStr.split(","));
-        }
-        return compressionConfig;
-    }
-
-    private static SSLEngineConfigurator getSslEngineConfigurator(ameba.Application app) {
-        SSLEngineConfigurator sslEngineConfigurator = null;
-        if (app.isSslConfigReady()) {
-            SSLContextConfigurator sslContextConfiguration = new SSLContextConfigurator();
-            sslContextConfiguration.setKeyPass(app.getSslKeyPassword());
-            sslContextConfiguration.setSecurityProtocol(app.getSslProtocol());
-
-            sslContextConfiguration.setKeyStoreBytes(app.getSslKeyStoreFile());
-            sslContextConfiguration.setKeyStorePass(app.getSslKeyStorePassword());
-            sslContextConfiguration.setKeyStoreProvider(app.getSslKeyStoreProvider());
-            sslContextConfiguration.setKeyStoreType(app.getSslKeyStoreType());
-            sslContextConfiguration.setKeyManagerFactoryAlgorithm(app.getSslKeyManagerFactoryAlgorithm());
-
-            sslContextConfiguration.setTrustStoreBytes(app.getSslTrustStoreFile());
-            if (StringUtils.isNotBlank(app.getSslTrustStorePassword()))
-                sslContextConfiguration.setTrustStorePass(app.getSslTrustStorePassword());
-            sslContextConfiguration.setTrustStoreType(app.getSslTrustStoreType());
-            sslContextConfiguration.setTrustStoreProvider(app.getSslTrustStoreProvider());
-            sslContextConfiguration.setTrustManagerFactoryAlgorithm(app.getSslTrustManagerFactoryAlgorithm());
-
-            sslEngineConfigurator = new SSLEngineConfigurator(sslContextConfiguration,
-                    app.isSslClientMode(), app.isSslNeedClientAuth(), app.isSslWantClientAuth());
-        }
-        return sslEngineConfigurator;
     }
 
     @Override
--- src/main/java/ameba/container/grizzly/server/GrizzlyServerFactory.java
+++ src/main/java/ameba/container/grizzly/server/GrizzlyServerFactory.java
@@ -1,6 +1,9 @@
 package ameba.container.grizzly.server;
 
 import ameba.container.grizzly.server.websocket.WebSocketAddOn;
+import ameba.server.Connector;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.glassfish.grizzly.GrizzlyFuture;
 import org.glassfish.grizzly.http.CompressionConfig;
 import org.glassfish.grizzly.http.ajp.AjpAddOn;
@@ -10,12 +13,11 @@ import org.glassfish.grizzly.http.server.NetworkListener;
 import org.glassfish.grizzly.http.server.ServerConfiguration;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
 import org.glassfish.grizzly.spdy.SpdyAddOn;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
 import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 import org.glassfish.grizzly.strategies.WorkerThreadIOStrategy;
 import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer;
-import org.glassfish.jersey.server.ContainerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.tyrus.core.TyrusWebSocketEngine;
 import org.glassfish.tyrus.core.Utils;
 import org.glassfish.tyrus.core.cluster.ClusterContext;
@@ -30,11 +32,7 @@ import javax.websocket.DeploymentException;
 import javax.websocket.server.ServerEndpointConfig;
 import javax.ws.rs.ProcessingException;
 import java.io.IOException;
-import java.net.URI;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -86,75 +84,137 @@ public class GrizzlyServerFactory {
      */
     public static final String MAX_SESSIONS_PER_REMOTE_ADDR = "app.websocket.maxSessionsPerRemoteAddr";
 
-    /**
-     * Creates HttpServer instance.
-     *
-     * @param uri                   URI on which the Jersey web application will be deployed. Only first path segment
-     *                              will be used as context path, the rest will be ignored.
-     * @param configuration         web application configuration.
-     * @param compressionCfg        {@link org.glassfish.grizzly.http.CompressionConfig} instance.
-     * @param secure                used for call {@link org.glassfish.grizzly.http.server.NetworkListener#setSecure(boolean)}.
-     * @param ajpEnabled            used for call {@link org.glassfish.grizzly.http.server.NetworkListener#registerAddOn(org.glassfish.grizzly.http.server.AddOn)}
-     *                              {@link org.glassfish.grizzly.spdy.SpdyAddOn}.
-     * @param jmxEnabled            {@link org.glassfish.grizzly.http.server.ServerConfiguration#setJmxEnabled(boolean)}.
-     * @param sslEngineConfigurator Ssl settings to be passed to {@link org.glassfish.grizzly.http.server.NetworkListener#setSSLEngineConfig(org.glassfish.grizzly.ssl.SSLEngineConfigurator)}.
-     * @param start                 if set to false, server will not get started, which allows to configure the
-     *                              underlying transport, see above for details.
-     * @return newly created {@link HttpServer}.
-     */
-    public static HttpServer createHttpServer(final URI uri,
-                                              final ResourceConfig configuration,
-                                              final CompressionConfig compressionCfg,
-                                              final boolean secure,
-                                              final boolean ajpEnabled,
-                                              final boolean jmxEnabled,
-                                              final SSLEngineConfigurator sslEngineConfigurator,
-                                              final boolean start) {
-        return createHttpServer(uri, ContainerFactory.createContainer(GrizzlyHttpContainer.class, configuration),
-                configuration.getProperties(), compressionCfg, secure, ajpEnabled, jmxEnabled, sslEngineConfigurator, start);
+    @SuppressWarnings("unchecked")
+    private static List<NetworkListener> createListeners(List<Connector> connectors, CompressionConfig compression) {
+        List<NetworkListener> listeners = Lists.newArrayList();
+
+        for (Connector connector : connectors) {
+            final String host = (connector.getHost() == null) ? NetworkListener.DEFAULT_NETWORK_HOST
+                    : connector.getHost();
+            final int port = (connector.getPort() == -1) ? 80 : connector.getPort();
+            final NetworkListener listener = new NetworkListener(
+                    StringUtils.defaultString(connector.getName(), DEFAULT_NETWORK_LISTENER_NAME),
+                    host,
+                    port);
+            listener.setSecure(connector.isSecureEnabled());
+            SSLEngineConfigurator sslEngineConfigurator = createSslEngineConfigurator(connector);
+            if (sslEngineConfigurator != null) {
+                listener.setSSLEngineConfig(sslEngineConfigurator);
+
+                if (connector.isSecureEnabled() && !connector.isAjpEnabled()) {
+                    SpdyAddOn spdyAddon = new SpdyAddOn();
+                    listener.registerAddOn(spdyAddon);
+                } else if (connector.isSecureEnabled()) {
+                    logger.warn("AJP模式开启，不启动SPDY支持");
+                }
+            }
+
+            if (connector.isAjpEnabled()) {
+                AjpAddOn ajpAddon = new AjpAddOn();
+                listener.registerAddOn(ajpAddon);
+            }
+
+            CompressionConfig compressionConfig = listener.getCompressionConfig();
+            CompressionConfig compressionCfg = createCompressionConfig((Map) connector.getRawProperties());
+
+            if (compressionCfg == null) {
+                compressionCfg = compression;
+            }
+
+            if (compressionCfg != null) {
+                compressionConfig.set(compressionCfg);
+            }
+            listeners.add(listener);
+        }
+
+        return listeners;
+    }
+
+    private static CompressionConfig createCompressionConfig(Map<String, Object> properties) {
+        CompressionConfig compressionConfig = null;
+        String modeStr = (String) properties.get("http.compression.mode");
+        if (StringUtils.isNotBlank(modeStr) && ((modeStr = modeStr.toUpperCase()).equals("ON") || modeStr.equals("FORCE"))) {
+
+            String minSizeStr = (String) properties.get("http.compression.minSize");
+            String mimeTypesStr = (String) properties.get("http.compression.mimeTypes");
+            String userAgentsStr = (String) properties.get("http.compression.ignore.userAgents");
+
+            compressionConfig = new CompressionConfig();
+            compressionConfig.setCompressionMode(CompressionConfig.CompressionMode.fromString(modeStr)); // the mode
+            if (StringUtils.isNotBlank(minSizeStr))
+                try {
+                    compressionConfig.setCompressionMinSize(Integer.parseInt(minSizeStr)); // the min amount of bytes to compress
+                } catch (Exception e) {
+                    logger.error("parse http.compression.minSize error", e);
+                }
+            if (StringUtils.isNotBlank(mimeTypesStr))
+                compressionConfig.setCompressableMimeTypes(mimeTypesStr.split(",")); // the mime types to compress
+            if (StringUtils.isNotBlank(userAgentsStr))
+                compressionConfig.setNoCompressionUserAgents(userAgentsStr.split(","));
+        }
+        return compressionConfig;
+    }
+
+    private static SSLEngineConfigurator createSslEngineConfigurator(Connector connector) {
+        SSLEngineConfigurator sslEngineConfigurator = null;
+        if (connector.isSslConfigReady()) {
+            SSLContextConfigurator sslContextConfiguration = new SSLContextConfigurator();
+            sslContextConfiguration.setKeyPass(connector.getSslKeyPassword());
+            sslContextConfiguration.setSecurityProtocol(connector.getSslProtocol());
+
+            sslContextConfiguration.setKeyStoreBytes(connector.getSslKeyStoreFile());
+            sslContextConfiguration.setKeyStorePass(connector.getSslKeyStorePassword());
+            sslContextConfiguration.setKeyStoreProvider(connector.getSslKeyStoreProvider());
+            sslContextConfiguration.setKeyStoreType(connector.getSslKeyStoreType());
+            sslContextConfiguration.setKeyManagerFactoryAlgorithm(connector.getSslKeyManagerFactoryAlgorithm());
+
+            sslContextConfiguration.setTrustStoreBytes(connector.getSslTrustStoreFile());
+            if (StringUtils.isNotBlank(connector.getSslTrustStorePassword()))
+                sslContextConfiguration.setTrustStorePass(connector.getSslTrustStorePassword());
+            sslContextConfiguration.setTrustStoreType(connector.getSslTrustStoreType());
+            sslContextConfiguration.setTrustStoreProvider(connector.getSslTrustStoreProvider());
+            sslContextConfiguration.setTrustManagerFactoryAlgorithm(connector.getSslTrustManagerFactoryAlgorithm());
+
+            sslEngineConfigurator = new SSLEngineConfigurator(
+                    sslContextConfiguration,
+                    connector.isSslClientMode(),
+                    connector.isSslNeedClientAuth(),
+                    connector.isSslWantClientAuth());
+        }
+        return sslEngineConfigurator;
     }
 
     /**
      * Creates HttpServer instance.
      *
-     * @param uri                   uri on which the {@link org.glassfish.jersey.server.ApplicationHandler} will be deployed. Only first path
-     *                              segment will be used as context path, the rest will be ignored.
-     * @param compressionCfg        {@link org.glassfish.grizzly.http.CompressionConfig} instance.
-     * @param handler               {@link org.glassfish.grizzly.http.server.HttpHandler} instance.
-     * @param secure                used for call {@link org.glassfish.grizzly.http.server.NetworkListener#setSecure(boolean)}.
-     * @param ajpEnabled            used for call {@link org.glassfish.grizzly.http.server.NetworkListener#registerAddOn(org.glassfish.grizzly.http.server.AddOn)}
-     *                              {@link org.glassfish.grizzly.spdy.SpdyAddOn}.
-     * @param jmxEnabled            {@link org.glassfish.grizzly.http.server.ServerConfiguration#setJmxEnabled(boolean)}.
-     * @param sslEngineConfigurator Ssl settings to be passed to {@link org.glassfish.grizzly.http.server.NetworkListener#setSSLEngineConfig(org.glassfish.grizzly.ssl.SSLEngineConfigurator)}.
-     * @param start                 if set to false, server will not get started, this allows end users to set
-     *                              additional properties on the underlying listener.
+     * @param handler    {@link org.glassfish.grizzly.http.server.HttpHandler} instance.
+     * @param connectors connectors
+     *                   {@link org.glassfish.grizzly.spdy.SpdyAddOn}.
+     * @param jmxEnabled {@link org.glassfish.grizzly.http.server.ServerConfiguration#setJmxEnabled(boolean)}.
+     * @param start      if set to false, server will not get started, this allows end users to set
+     *                   additional properties on the underlying listener.
      * @return newly created {@link HttpServer}.
      * @throws javax.ws.rs.ProcessingException
      * @see GrizzlyHttpContainer
      */
     @SuppressWarnings("unchecked")
-    public static HttpServer createHttpServer(final URI uri,
-                                              final HttpHandler handler,
-                                              final Map<String, Object> webSocketConfig,
-                                              final CompressionConfig compressionCfg,
-                                              final boolean secure,
-                                              final boolean ajpEnabled,
+    public static HttpServer createHttpServer(final HttpHandler handler,
+                                              final Map<String, Object> properties,
+                                              List<Connector> connectors,
                                               final boolean jmxEnabled,
-                                              final SSLEngineConfigurator sslEngineConfigurator,
                                               final boolean start)
             throws ProcessingException {
-        final String host = (uri.getHost() == null) ? NetworkListener.DEFAULT_NETWORK_HOST
-                : uri.getHost();
-        final int port = (uri.getPort() == -1) ? 80 : uri.getPort();
-        final NetworkListener listener = new NetworkListener(DEFAULT_NETWORK_LISTENER_NAME, host, port);
 
-        final ServerContainer webSocketContainer = bindWebSocket(webSocketConfig, listener);
+        List<NetworkListener> listeners = createListeners(connectors, createCompressionConfig(properties));
+
+        final ServerContainer webSocketContainer = bindWebSocket(properties, listeners);
+
         final HttpServer server = new HttpServer() {
             @Override
             public synchronized void start() throws IOException {
                 if (webSocketContainer != null)
                     try {
-                        webSocketContainer.start("/", port);
+                        webSocketContainer.start("/", -1);
                     } catch (DeploymentException e) {
                         logger.error("启动websocket容器失败", e);
                     }
@@ -177,35 +237,15 @@ public class GrizzlyServerFactory {
         };
 
 
-        listener.setSecure(secure);
-        if (sslEngineConfigurator != null) {
-            listener.setSSLEngineConfig(sslEngineConfigurator);
-
-            if (secure && !ajpEnabled) {
-                SpdyAddOn spdyAddon = new SpdyAddOn();
-                listener.registerAddOn(spdyAddon);
-            } else if (secure) {
-                logger.warn("AJP模式开启，不启动SPDY支持");
-            }
-        }
-
-        if (ajpEnabled) {
-            AjpAddOn ajpAddon = new AjpAddOn();
-            listener.registerAddOn(ajpAddon);
-        }
-
         server.getServerConfiguration().setJmxEnabled(jmxEnabled);
 
-        server.addListener(listener);
-        CompressionConfig compressionConfig = listener.getCompressionConfig();
-        if (compressionCfg != null) {
-            compressionConfig.set(compressionCfg);
+        for (NetworkListener listener : listeners) {
+            server.addListener(listener);
         }
-
         // Map the path to the processor.
         final ServerConfiguration config = server.getServerConfiguration();
         if (handler != null) {
-            config.addHttpHandler(handler, uri.getPath());
+            config.addHttpHandler(handler);
         }
 
         config.setPassTraceRequest(true);
@@ -224,7 +264,7 @@ public class GrizzlyServerFactory {
         return server;
     }
 
-    private static ServerContainer bindWebSocket(Map<String, Object> properties, final NetworkListener listener) {
+    private static ServerContainer bindWebSocket(Map<String, Object> properties, final List<NetworkListener> listeners) {
 
         final Map<String, Object> localProperties;
         // defensive copy
@@ -240,7 +280,7 @@ public class GrizzlyServerFactory {
         final Integer maxSessionsPerApp = Utils.getProperty(localProperties, MAX_SESSIONS_PER_APP, Integer.class);
         final Integer maxSessionsPerRemoteAddr = Utils.getProperty(localProperties, MAX_SESSIONS_PER_REMOTE_ADDR, Integer.class);
 
-        return new TyrusServerContainer((Set<Class<?>>)null) {
+        return new TyrusServerContainer((Set<Class<?>>) null) {
 
             private final WebSocketEngine engine = TyrusWebSocketEngine.builder(this)
                     .incomingBufferSize(incomingBufferSize)
@@ -274,9 +314,11 @@ public class GrizzlyServerFactory {
                 ThreadPoolConfig workerThreadPoolConfig = Utils.getProperty(localProperties, WORKER_THREAD_POOL_CONFIG, ThreadPoolConfig.class);
                 ThreadPoolConfig selectorThreadPoolConfig = Utils.getProperty(localProperties, SELECTOR_THREAD_POOL_CONFIG, ThreadPoolConfig.class);
 
+                TCPNIOTransportBuilder transportBuilder = null;
+
                 // TYRUS-287: configurable server thread pools
                 if (workerThreadPoolConfig != null || selectorThreadPoolConfig != null) {
-                    TCPNIOTransportBuilder transportBuilder = TCPNIOTransportBuilder.newInstance();
+                    transportBuilder = TCPNIOTransportBuilder.newInstance();
                     if (workerThreadPoolConfig != null) {
                         transportBuilder.setWorkerThreadPoolConfig(workerThreadPoolConfig);
                     }
@@ -284,15 +326,22 @@ public class GrizzlyServerFactory {
                         transportBuilder.setSelectorThreadPoolConfig(selectorThreadPoolConfig);
                     }
                     transportBuilder.setIOStrategy(WorkerThreadIOStrategy.getInstance());
-                    listener.setTransport(transportBuilder.build());
-                } else {
-                    // if no configuration is set, just update IO Strategy to worker thread strat.
-                    listener.getTransport().setIOStrategy(WorkerThreadIOStrategy.getInstance());
                 }
 
-                // idle timeout set to indefinite.
-                listener.getKeepAlive().setIdleTimeoutInSeconds(-1);
-                listener.registerAddOn(new WebSocketAddOn(this));
+                WebSocketAddOn addOn = new WebSocketAddOn(this);
+
+                for (NetworkListener listener : listeners) {
+
+                    if (transportBuilder != null) {
+                        listener.setTransport(transportBuilder.build());
+                    } else {
+                        listener.getTransport().setIOStrategy(WorkerThreadIOStrategy.getInstance());
+                    }
+
+                    // idle timeout set to indefinite.
+                    listener.getKeepAlive().setIdleTimeoutInSeconds(-1);
+                    listener.registerAddOn(addOn);
+                }
 
                 if (applicationEventListener != null) {
                     applicationEventListener.onApplicationInitialized(rootPath);
